c3bf7b9b1c20dd9d6ce3bdf809f88b60ebec68ef
Yvs08 <yveslambenou@gmail.com>
2021-03-04 22:50:10 +0100

    Update FizzBuz.java

--- src/FizzBuz.java
+++ src/FizzBuz.java
@@ -17,9 +17,9 @@ public class FizzBuz {
             {
                 if (((i % 3) == 0) && ((i % 5) == 0)) // multiple de 3 et 5
                     System.out.println("fizzbuzz");
-                else if ((i % 3) == 0) // multiple de 3?
+                else if ((i % 3) == 0) // multiple de 3
                     System.out.println("fizz");
-                else if ((i % 5) == 0) //  multiple de 5?
+                else if ((i % 5) == 0) //  multiple de 5
                     System.out.println("buzz");
                 else
                     System.out.println(i); //  cas écheant
